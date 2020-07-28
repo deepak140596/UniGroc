042358cb67387d7c74412803f5596dca53706d60
ahrar01 <ahraralam2012@gmail.com>
2020-07-29 02:00:39 +0530

    added animation ratingBar

--- app/src/main/java/com/avvnapps/unigroc/ui/Activity/AddReviewActivity.kt
+++ app/src/main/java/com/avvnapps/unigroc/ui/Activity/AddReviewActivity.kt
@@ -1,12 +1,34 @@
 package com.avvnapps.unigroc.ui.Activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.avvnapps.unigroc.R
+import com.avvnapps.unigroc.utils.hide
+import kotlinx.android.synthetic.main.activity_add_review.*
+
 
 class AddReviewActivity : AppCompatActivity() {
+
+    val TAG by lazy { "AddReviewActivity" }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_review)
+        close_add_review_activity.setOnClickListener {
+            onBackPressed()
+        }
+
+        RatingBar.setOnRatingChangeListener { ratingBar, rating, fromUser ->
+            ratingBar.setMinimumStars(1.0F)
+            imageReviewRL.hide()
+            Log.e(TAG, "onRatingChange: $rating")
+        }
+
     }
+
+
 }
+
+
